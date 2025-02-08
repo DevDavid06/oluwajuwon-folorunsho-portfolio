791192b93c6b722cdb8e80079610c03cbcebef81
DevDavid06 <iamjaybest@gmail.com>
2025-02-08 04:06:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-function showSidebar(){
+function showSideBar(){
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'flex'
+    document.body.style.overflow = 'hidden'; // Disable background scrolling
+
 }
 function hideSidebar(){
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none';
+    document.body.style.overflow = ''; // Re-enable background scrolling
 };
-//form
+
+document.addEventListener('DOMContentLoaded', function () {
+    const sidebarLinks = document.querySelectorAll('.sidebar a'); // Select all sidebar links
+
+    sidebarLinks.forEach(link => {
+        link.addEventListener('click', function (event) {
+            hideSidebar(); // Close the sidebar
+        });
+    });
+});
+
+
+
 (function() {
-    emailjs.init('ba4l4eheXe6inDRPO'); // Replace with your actual public key
+    emailjs.init('ba4l4eheXe6inDRPO'); 
 })();
 
 
-// Add Event Listener on Form Submission
 document.addEventListener('DOMContentLoaded', function(){
     const form = document.getElementById('contact-form');
     const responseDiv = document.getElementById('formResponse');
@@ -20,13 +34,13 @@ document.addEventListener('DOMContentLoaded', function(){
     form.addEventListener('submit', function(event) {
         event.preventDefault();
         
-        responseDiv.style.color = "blue";
+        responseDiv.style.color = "white";
         responseDiv.textContent = "Submitting...";
 
         emailjs.sendForm('service_b21qk9s', 'template_zfutxds', form)
             .then(() => {
-                responseDiv.style.color = "green";
-                responseDiv.textContent = "Request sent successfully!";
+                responseDiv.style.color = "white";
+                responseDiv.textContent = "Message sent successfully!";
                 form.reset();
                 setTimeout(() => {
                 responseDiv.textContent = "";
